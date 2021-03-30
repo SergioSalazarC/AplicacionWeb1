f1fb9e61e5de94e71b825a8e84a0c08234d113d8
serxu <59147261+serxu@users.noreply.github.com>
2021-03-30 12:18:10 +0200

    proyecto creado

---
+++ PracticaFilosanitarios/src/main/java/com/example/demo/PracticaFilosanitariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaFilosanitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaFilosanitariosApplication.class, args);
+	}
+
+}
